b16a2c1a99450dc46fcb1b7fc7add63f48cef87c
sdcb <sdflysha@qq.com>
2023-10-31 13:20:47 +0800

    update to latest deps

--- mini-openvino-paddleocr/Program.cs
+++ mini-openvino-paddleocr/Program.cs
@@ -3,10 +3,11 @@ using Sdcb.OpenVINO.PaddleOCR.Models.Online;
 using Sdcb.OpenVINO.PaddleOCR.Models;
 using Sdcb.OpenVINO.PaddleOCR;
 using System.Diagnostics;
-using System;
 
 FullOcrModel model = await OnlineFullModels.ChineseV4.DownloadAsync();
 
+// Load local file by following code:
+// using Mat src = Cv2.ImRead(@"C:\test.jpg");
 using Mat src = Cv2.ImDecode(await new HttpClient().GetByteArrayAsync("https://io.starworks.cc:88/paddlesharp/ocr/samples/xdr5450.webp"), ImreadModes.Color);
 
 using (PaddleOcrAll all = new(model)
@@ -15,8 +16,6 @@ using (PaddleOcrAll all = new(model)
     Enable180Classification = true,
 })
 {
-    // Load local file by following code:
-    // using (Mat src2 = Cv2.ImRead(@"C:\test.jpg"))
     Stopwatch sw = Stopwatch.StartNew();
     PaddleOcrResult result = all.Run(src);
     Console.WriteLine($"elapsed={sw.ElapsedMilliseconds} ms");
